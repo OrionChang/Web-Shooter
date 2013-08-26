d2a8b86b9cd88f3affdd31491923ab25c458ef19
Orion Chang <ism.zjx@gmail.com>
2013-08-26 08:46:18 +0700

    init with devise installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebshooterV1::Application.config.secret_key_base = 'fa3ffadb3aaefa5f1887a0f4d1e60da5a54f8487f64fd85b1e74fb655841b11a91bf4e604723e75a7a730e59f4c3583b37bd57cf04ede4553f1483ba4d948bbe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WebshooterV1::Application.config.session_store :cookie_store, key: '_webshooter_v1_session'
