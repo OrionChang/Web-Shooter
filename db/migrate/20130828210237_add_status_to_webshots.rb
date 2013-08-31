313cec495c6ee3fff35ce88a19bebcc5e3c17d10
Orion Chang <ism.zjx@gmail.com>
2013-08-31 23:30:05 +0700

    prototype

--- db/migrate/20130828210237_add_status_to_webshots.rb
+++
@@ -1,6 +0,0 @@
-class AddStatusToWebshots < ActiveRecord::Migration
-  def change
-    add_column :webshots, :saved, :boolean, :default => false
-
-  end
-end
--- db/migrate/20130829041445_add_shares_to_webshots.rb
+++
@@ -1,5 +0,0 @@
-class AddSharesToWebshots < ActiveRecord::Migration
-  def change
-    add_column :webshots, :shares, :integer, :default => 0
-  end
-end
